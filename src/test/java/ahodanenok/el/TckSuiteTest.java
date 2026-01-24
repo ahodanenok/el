8256b9eb3f6b03b66cd1139fdb053ebf5efc81cf
Andrei Hodanenok <xoda91@gmail.com>
2026-01-24 20:37:10 +0300

    Tck tests passed

--- src/test/java/ahodanenok/el/TckSuiteTest.java
+++ src/test/java/ahodanenok/el/TckSuiteTest.java
@@ -4,7 +4,7 @@ import org.junit.platform.suite.api.IncludeClassNamePatterns;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
-//@Suite
+@Suite
 @SelectPackages("com.sun.ts.tests.el")
 @IncludeClassNamePatterns(".*")
 public class TckSuiteTest { }
